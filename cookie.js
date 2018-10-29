86fb18d3d2b82ac41a75f88ad2f53a687f975ded
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-10-29 15:04:03 +0100

    cookie done

--- cookie.js
+++ cookie.js
@@ -8,6 +8,12 @@ let BonusAutoSeuil = 300;
 //cout du bonus de l'auto click
 let AutoClickBonus = 0;
 //bonus de base à + 0 logique
+let Bonus30 = 0;
+//0 car le bonus va être à 1 et donc doubler la valeur de base qui est 1
+let Bonus30Price = 500; 
+//Prix de base pour l'achat du bonus
+let BonusTimer = 0; 
+let DoIt = false;
 
 const cookie = document.getElementById("cookie");
 //stock l'id de l'image du cookie dans la variable cookie
@@ -17,10 +23,11 @@ const autoC = document.getElementById("autoC");
 //stock l'id du boutton autoC dans la variable 
 const autoBonus = document.getElementById("autoBonus");
 //stock l'id du boutton autoC dans la variable
+const bonus = document.getElementById("Bonus");
 
 cookie.addEventListener("click", function(){
     //au clique sur l'image 
-    score += (1*multiplicateur);
+    score += ((1+Bonus30)*multiplicateur);
     //augmente le score de 1
     function affiche(){
         return document.getElementById("score").innerHTML = score;
@@ -46,7 +53,31 @@ cookie.addEventListener("click", function(){
     }
     affiche();
 });
-
+//bonus x 2 pendant 30 sec 
+bonus.addEventListener("click", function(){
+    if (score >= Bonus30Price && BonusTimer <= 30){
+        score -=  Bonus30Price;
+        Bonus30 = 1;
+        DoIt = true;
+        if (DoIt){
+            setInterval(function(){
+                if (BonusTimer < 5 && DoIt){
+                    BonusTimer += 1;           
+                    bonus.innerHTML = BonusTimer;
+                    bonus.innerHTML = "Bonus * 2 activé "+BonusTimer;
+                }
+                else if (BonusTimer >= 30){
+                    Bonus30 = 0; 
+                    BonusTimer = 0;
+                    Bonus30Price *= 2;
+                    bonus.innerHTML = Bonus30Price+" cookies pour obtenir cookies *2 sur 30 sec !";
+                    DoIt = false;
+                }
+            }, 1000);
+        }
+    }
+});
+//multiplicateur de base
 multi.addEventListener("click", function(){
     if (score >= multiPrice){
         multiplicateur += 1;
@@ -55,34 +86,8 @@ multi.addEventListener("click", function(){
         multi.innerHTML = "Multiplicateur +"+multiplicateur+" Prochaine niveau accessible pour "+multiPrice+" cookies";    
         return  multiplicateur;
     }
-    else {
-        multi.innerHTML = "Encore "+(multiPrice-score)+" cookies avant le bonus"; 
-    }
 });
-/*
-function autoclikeur(){
-    setInterval(function(){
-        if (autoClick == 0 && autoClikActivation){
-            score += 1*multiplicateur;
-            //augmente le score de 1*multi
-            document.getElementById("score").innerHTML = score;
-            //affiche le score sur le p dans le hmtl
-            autoClick = 1;
-            return score; 
-        }
-        else if (autoClick == 1 && autoClikActivation){
-            autoC.innerHTML = "Auto-Clik Activé";
-            autoClick = 0;
-        }
-        else if (score == ActivationSeuil && autoClikActivation == false){
-            score -= ActivationSeuil;
-            autoClikActivation = true;
-        }
-        }
-    , 500);// 1000 = 1 sec
-}
-autoclikeur();
-*/
+//autoCliqueur
 autoC.addEventListener("click", function(){
     setInterval(function(){
         if (autoClick == 0 && autoClikActivation){
@@ -107,7 +112,7 @@ autoC.addEventListener("click", function(){
             autoC.innerHTML = "Auto-Clik Activé level 1";
             //active le bouton
         }
-        }
+    }
     , 500);// 1000 = 1 sec
 });
 //bonus de l'auto-cliqueur
@@ -120,7 +125,7 @@ autoBonus.addEventListener("click", function(){
         //autoBonus.disabled = false;
         //active le bouton
         autoC.innerHTML = ("Auto-Clik Activé level "+(1+AutoClickBonus));
-        autoBonus.innerHTML =("Prochain bonus de click pour "+BonusAutoSeuil+" cookies")
+        autoBonus.innerHTML =("Prochain bonus de click pour "+BonusAutoSeuil+" cookies");
     }
     else if(autoClikActivation && score >= BonusAutoSeuil && AutoClickBonus > 0){
         score -= BonusAutoSeuil;
@@ -128,6 +133,6 @@ autoBonus.addEventListener("click", function(){
         BonusAutoSeuil *= 2; 
         //augmentation du seuil du bonus * 2 
         autoC.innerHTML = ("Auto-Clik Activé level "(1+AutoClickBonus));
-        autoBonus.innerHTML =("Prochain bonus de click pour "+BonusAutoSeuil+" cookies")
+        autoBonus.innerHTML =("Prochain bonus de click pour "+BonusAutoSeuil+" cookies");
     }
 });
