1e1d711eb170549170249f3ffcb52958ebe3d591
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-11-21 14:58:43 +0100

    terminé

--- challenge.js
+++ challenge.js
@@ -1,5 +1,3 @@
-d = "#";
-e = " ";
 de = "# ";
 ed = " #";
 let result = "";
@@ -7,12 +5,10 @@ let result2 = "";
 let final = "";
 
 function test(x,y){
-    for (let i=0; i<x; i++){
+    for (let i=0; i<x; i+=2){
         result = result + de;
         result2 = result2 + ed;
     }
-    //console.log(result); 
-    //console.log(result2); 
     for (let j=0; j<y; j+=2){
         final = final +"\n"+ result +"\n"+ result2;
     }
@@ -20,6 +16,6 @@ function test(x,y){
 
 };
 
-test(8,8);
+test(6,4);
 
 
