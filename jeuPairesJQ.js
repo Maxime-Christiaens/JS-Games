62e8ec843dd880bad3253b4d660937499c4ccc05
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-12-05 17:27:59 +0100

    push push

--- jeuPairesJQ.js
+++ jeuPairesJQ.js
@@ -1,22 +1,42 @@
 $(function () {//permet d'exécuter le code quand toute la page est 100% chargée.
-    let ListeImgs = ["imgs/anehihan.jpg", "imgs/chatminou.jpg", "imgs/chientoutou.jpg", "imgs/lamacrachat.jpg", "imgs/lapinscrottes.jpg", "imgs/lionnegraou.jpg", "imgs/oursbaby.jpg", "imgs/anehihan.jpg", "imgs/chatminou.jpg", "imgs/chientoutou.jpg", "imgs/lamacrachat.jpg", "imgs/lapinscrottes.jpg", "imgs/lionnegraou.jpg", "imgs/oursbaby.jpg"];
+/*     let ListeImgs = ["imgs/anehihan.jpg", "imgs/chatminou.jpg", "imgs/chientoutou.jpg", "imgs/lamacrachat.jpg", "imgs/lapinscrottes.jpg", "imgs/lionnegraou.jpg", "imgs/oursbaby.jpg", "imgs/anehihan.jpg", "imgs/chatminou.jpg", "imgs/chientoutou.jpg", "imgs/lamacrachat.jpg", "imgs/lapinscrottes.jpg", "imgs/lionnegraou.jpg", "imgs/oursbaby.jpg"]; */
+let ListeImgs = ["imgs/anehihan.jpg", "imgs/chatminou.jpg","imgs/anehihan.jpg", "imgs/chatminou.jpg","imgs/lionnegraou.jpg"]
 
-    ListeImgs.sort(() => 0.5 - Math.random());
-    //randomise la liste
+    /*     ListeImgs.sort(() => 0.5 - Math.random());
+        //randomise la liste
+    
+        for (let i = 0; i < 14; i++) {
+            $("#img" + i).attr("src", ListeImgs[i]);
+        } */
 
-    for (let i = 0; i < 14; i++) {
-        $("#img" + i).attr("src", ListeImgs[i]);
-    }
+    let first = null;
+    let second = null;
 
-    $(".Jpaires").click(function (event) {
-        $(".Jpaires").toggleClass("flip");
-        $('.Jpaires')
-            .delay(200)
-            .queue(function (next) {
-                $(".Jpaires").css("background-image", "url(imgs/anehihan.jpg)");
-            });
+    $(".arrayImgs").click(function (event) {
+        if (second == null && first != null) {
+            second = event.target;
+            let secondNumber = second.className;
+            console.log(event.target);
+            $(second).toggleClass("flip");
+            //active l'animation au clique
+            $(second)
+                .delay(200)
+                .queue(function (next) {
+                    $(second).css("background-image", "url(" + ListeImgs[secondNumber] + ")");
+                });
+        }
+        else {
+            first = (event.target);
+            let firstNumber = first.className;
+            console.log("first = " + firstNumber);
+            $(first).toggleClass("flip");
+            //active l'animation au clique
+            $(first)
+                .delay(200)
+                .queue(function (next) {
+                    $(first).css("background-image", "url(" + ListeImgs[firstNumber] + ")");
+                });
+        }
+    })
 });
-})
 
-//document.getElementById(clicked.id)
-//test
